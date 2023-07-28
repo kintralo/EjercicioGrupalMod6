7b78f129e3bd4f166021dfe759e29d9048ee2c3b
Pablov <juan.vasquez@valposystem.com>
2023-07-27 22:15:50 -0400

    Creación de jsp

---
+++ ejerciciosGrupal/src/main/java/controladores/ServletContacto.java
@@ -0,0 +1,35 @@
+package controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ServletContacto  extends HttpServlet {
+
+    /**
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    /**
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
