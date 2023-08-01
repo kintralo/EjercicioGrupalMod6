e9c8777b6b903b526de2fd358305f4a1f6256095
Pablov <juan.vasquez@valposystem.com>
2023-07-31 21:33:11 -0400

    Creación de proyecto con Sprint

---
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/EjercicioGrupalSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.adt.ejerciciogrupalsprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioGrupalSprintBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioGrupalSprintBootApplication.class, args);
+    }
+
+}
---
+++ ejercicioGrupalSprintBoot/src/test/java/com/adt/ejerciciogrupalsprintboot/EjercicioGrupalSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adt.ejerciciogrupalsprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioGrupalSprintBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
