ca0ac4c603e3a6ff3be4d513b75b37ec6c8b6c37
Pablov <juan.vasquez@valposystem.com>
2023-08-14 20:50:15 -0400

    Agregar comentarios de creación IContactoRepository.java

--- ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/repositories/IContactoRepository.java
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/repositories/IContactoRepository.java
@@ -3,7 +3,14 @@ package com.adt.ejerciciogrupalsprintboot.repositories;
 import com.adt.ejerciciogrupalsprintboot.models.Contacto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+ * @author  Juan Pablo Vásquez
+ * Ándres Tapia
+ * Victor Briso
+ * Sebastián Araya
+ * Vannya Riffo
+ * @version 1.0
+ */
 @Repository
 public interface IContactoRepository extends JpaRepository<Contacto, Long> {
 }
