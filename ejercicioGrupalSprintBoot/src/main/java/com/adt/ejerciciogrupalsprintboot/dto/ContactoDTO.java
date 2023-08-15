6b77d94cb42e021fdee1429918988d0e952e11fc
Pablov <juan.vasquez@valposystem.com>
2023-08-14 20:54:38 -0400

    Agregar las importaciones de Lombok y creación de atributos de clase ContactoDTO.java

---
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/dto/ContactoDTO.java
@@ -0,0 +1,34 @@
+package com.adt.ejerciciogrupalsprintboot.dto;
+
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+
+/**
+ * @author  Juan Pablo Vásquez
+ * Ándres Tapia
+ * Victor Briso
+ * Sebastián Araya
+ * Vannya Riffo
+ * @version 1.0
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ContactoDTO {
+
+    private Long id_contacto;
+
+    private String contacto_nombre;
+
+    private String contacto_email;
+
+    private String contacto_mensaje;
+
+}
