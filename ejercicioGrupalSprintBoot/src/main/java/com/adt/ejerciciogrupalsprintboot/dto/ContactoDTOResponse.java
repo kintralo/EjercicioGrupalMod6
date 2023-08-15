f9f14d260522c51dd6f62236ec4e6e2c47f54cb4
Pablov <juan.vasquez@valposystem.com>
2023-08-14 21:44:31 -0400

    Creación de clase ContactoDTOResponse.java para la lista de contactos

---
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/dto/ContactoDTOResponse.java
@@ -0,0 +1,33 @@
+package com.adt.ejerciciogrupalsprintboot.dto;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+
+/**
+ * @author  Juan Pablo Vásquez
+ * Ándres Tapia
+ * Victor Briso
+ * Sebastián Araya
+ * Vannya Riffo
+ * @version 1.0
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ContactoDTOResponse {
+
+    private Long id_contacto;
+
+    private String contacto_nombre;
+
+    private String contacto_email;
+
+    private String contacto_mensaje;
+
+}
