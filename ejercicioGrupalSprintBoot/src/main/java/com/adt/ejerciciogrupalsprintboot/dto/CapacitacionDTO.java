35af0e2460a1649cead8ba0afff32e91e011ffc2
Pablov <juan.vasquez@valposystem.com>
2023-08-16 19:50:54 -0400

    Eliminar importaciones que no se utilizan

--- ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/dto/CapacitacionDTO.java
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/dto/CapacitacionDTO.java
@@ -3,7 +3,6 @@ package com.adt.ejerciciogrupalsprintboot.dto;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
