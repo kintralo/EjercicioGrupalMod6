7ff07e0b67b1f8fb32001c6603118a3fd6084223
Pablov <juan.vasquez@valposystem.com>
2023-08-25 19:42:08 -0400

    Modificación en método

--- ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/services/implementaciones/IPagoServices.java
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/services/implementaciones/IPagoServices.java
@@ -16,11 +16,11 @@ import java.util.List;
 public interface IPagoServices {
     /**
      *
-     * @param pagoDTOResponse
+     * @param pagoDTORequest
      * @return
      * @throws Exception
      */
-    public PagoDTORequest createPago(PagoDTOResponse pagoDTOResponse) throws Exception;
+    public PagoDTORequest createPago(PagoDTORequest pagoDTORequest) throws Exception;
 
     /**
      *
