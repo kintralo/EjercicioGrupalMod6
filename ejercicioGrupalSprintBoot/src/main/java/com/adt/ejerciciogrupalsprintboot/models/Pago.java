a82df43e03f4f3be8934c03ce84f507c5630e8f4
Pablov <juan.vasquez@valposystem.com>
2023-08-24 22:52:10 -0400

    Creación de relación

--- ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/models/Pago.java
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/models/Pago.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 
 import lombok.NoArgsConstructor;
 import lombok.Getter;
@@ -39,9 +41,13 @@ public class Pago {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pago")
-    private Integer id;
+    private Integer id_pago;
+
+    @ManyToOne()
+    @JoinColumn()
     @Column(name = "cliente_id")
-    private Integer cliente_id;
+    private Usuarios usuarios;
+
     @Column(name = "id_pago")
     private Double monto;
     @Column(name = "fecha_pago")
