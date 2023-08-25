edc92ca247bd4a064ff1666c76d5c58989835cc8
Pablov <juan.vasquez@valposystem.com>
2023-08-24 22:12:10 -0400

    Agregar importaciones

--- ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/models/Usuarios.java
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/models/Usuarios.java
@@ -1,5 +1,12 @@
 package com.adt.ejerciciogrupalsprintboot.models;
-import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 
 import lombok.NoArgsConstructor;
 import lombok.Getter;
