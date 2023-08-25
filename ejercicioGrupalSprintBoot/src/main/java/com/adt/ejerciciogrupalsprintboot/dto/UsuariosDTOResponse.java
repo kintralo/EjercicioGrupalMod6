501fb19050059818869c4dd3487a8cb53d3ae85e
Pablov <juan.vasquez@valposystem.com>
2023-08-24 22:21:28 -0400

    Agregar importaciones

--- ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/dto/UsuariosDTOResponse.java
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/dto/UsuariosDTOResponse.java
@@ -1,7 +1,23 @@
 package com.adt.ejerciciogrupalsprintboot.dto;
 
 
-import lombok.*;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalTime;
+
+/**
+ * @author  Juan Pablo Vásquez
+ * Ándres Tapia
+ * Victor Briso
+ * Sebastián Araya
+ * Vannya Riffo
+ * @version 1.0
+ */
+
 
 @Getter
 @Setter
