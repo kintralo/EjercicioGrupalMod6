d52884f1264042cf4b716c044690ad13e698a5a0
Pablov <juan.vasquez@valposystem.com>
2023-08-24 23:01:17 -0400

    Creación de los métodos y  comentarios

---
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/services/PagoServicesImp.java
@@ -0,0 +1,37 @@
+package com.adt.ejerciciogrupalsprintboot.services;
+
+import com.adt.ejerciciogrupalsprintboot.dto.PagoDTORequest;
+import com.adt.ejerciciogrupalsprintboot.dto.PagoDTOResponse;
+import com.adt.ejerciciogrupalsprintboot.services.implementaciones.IPagoServices;
+
+import java.util.List;
+/**
+ * @author Juan Pablo Vásquez
+ * Ándres Tapia
+ * Victor Briso
+ * Sebastián Araya
+ * Vannya Riffo
+ * @version 1.0
+ */
+public class PagoServicesImp implements IPagoServices {
+    /**
+     *
+     * @param pagoDTOResponse
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public PagoDTORequest createPago(PagoDTOResponse pagoDTOResponse) throws Exception {
+        return null;
+    }
+
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public List<PagoDTOResponse> listPago() throws Exception {
+        return null;
+    }
+}
