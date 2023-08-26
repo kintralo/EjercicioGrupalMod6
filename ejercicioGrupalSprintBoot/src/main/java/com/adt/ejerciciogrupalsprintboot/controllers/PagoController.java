0cdf49fcb55baf794db91d7c065d9ea34f0d63de
Pablov <juan.vasquez@valposystem.com>
2023-08-25 20:16:28 -0400

    Creación de Lógica

--- ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/controllers/PagoController.java
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/controllers/PagoController.java
@@ -1,4 +1,48 @@
 package com.adt.ejerciciogrupalsprintboot.controllers;
+import com.adt.ejerciciogrupalsprintboot.dto.PagoDTORequest;
+import com.adt.ejerciciogrupalsprintboot.dto.PagoDTOResponse;
+import com.adt.ejerciciogrupalsprintboot.services.implementaciones.IPagoServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author  Juan Pablo Vásquez
+ * Ándres Tapia
+ * Victor Briso
+ * Sebastián Araya
+ * Vannya Riffo
+ * @version 1.0
+ */
+@Controller
 public class PagoController {
+    @Autowired
+    private IPagoServices iPagoServices;
+
+    @PostMapping
+    public String createPago(@Param("")Integer usuario,
+                             @RequestParam("")Double monto,
+                             @RequestParam("") Date fecha_pago,
+                             Model model)throws Exception{
+        PagoDTORequest pagoDTORequest= new PagoDTORequest(
+                usuario,monto,fecha_pago);
+        iPagoServices.createPago( pagoDTORequest);
+        List<PagoDTOResponse> pagoDTOResponses= iPagoServices.listPago();
+        model.addAttribute("listaPago", pagoDTOResponses);
+        return "/listaPagosView";
+    }
+
+    @GetMapping("/listaPagos")
+    public String getPagos(Model model)throws Exception{
+        List<PagoDTOResponse> pagoDTOResponses= iPagoServices.listPago();
+        model.addAttribute("listaPago", pagoDTOResponses);
+        return "/listaPagosView";
+    }
 }
