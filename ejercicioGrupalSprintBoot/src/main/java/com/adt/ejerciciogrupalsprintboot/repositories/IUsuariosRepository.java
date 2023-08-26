b7ebc1543348b41be412954d3658bb5cb22ef36b
Pablov <juan.vasquez@valposystem.com>
2023-08-25 20:44:00 -0400

    Modificación de A entero

--- ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/repositories/IUsuariosRepository.java
+++ ejercicioGrupalSprintBoot/src/main/java/com/adt/ejerciciogrupalsprintboot/repositories/IUsuariosRepository.java
@@ -4,5 +4,5 @@ package com.adt.ejerciciogrupalsprintboot.repositories;
 import com.adt.ejerciciogrupalsprintboot.models.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUsuariosRepository extends JpaRepository<Usuarios, Long> {
+public interface IUsuariosRepository extends JpaRepository<Usuarios, Integer> {
 }
